3d7ca2f3ace6550e03d0007650b135ac73a72728
Alexandre Silva <52967992+AlexandreSilva1608@users.noreply.github.com>
2021-03-06 14:45:17 -0300

    Primeiro commit do projeto

---
+++ audiflow/src/test/java/com/project/audiflow/AudiflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.audiflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
